427fe8fbc30ab19ed77266836c66b23ebb0814ec
VivekPatil2009 <vivek.patil@splendidinfosystems.com>
2026-09-19 18:38:11 +0530

    add Logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
-// import {start} from 'repl'
+// import { start } from "repl";
+
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/Logger";
 
 const startServer = () => {
   const PORT = Config.PORT;
+
   try {
-    app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
+    app.listen(PORT, () => {
+      logger.error("testing error..");
+      logger.info("server Listening on port", { Port: PORT });
+    });
   } catch (err) {
     console.error(err);
     process.exit(1);
